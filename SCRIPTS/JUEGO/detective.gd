b79bfb55b248937a993f37c46b1972d3c0764bfe
Claudia <149967204+wommiez@users.noreply.github.com>
2024-06-18 14:57:31 +0200

    salud detective

--- SCRIPTS/JUEGO/detective.gd
+++ SCRIPTS/JUEGO/detective.gd
@@ -1,8 +1,26 @@
 extends CharacterBody2D
-
+class_name Player
 
 const speed = 300.0
 var dir = "S"
+
+#ajustar valor de la salud que da una poción
+@export var potionhealth = 1
+@export var maxHealth = 30
+@onready var currentHealth: int = maxHealth
+
+signal healthChanged #emitir señal cuando cambia salud a barra de progreso
+
+#actualizar salud al tomar pocióm
+func increaseHealth():
+	if currentHealth>=maxHealth: return
+	currentHealth += potionhealth
+	healthChanged.emit()
+	
+
+func hurtbyenemy():
+	healthChanged.emit()
+	
 
 func _physics_process(delta):
 	player_movement(delta)
