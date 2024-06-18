7e3d017273a79dbadee1bdb86c904065a0783e46
Claudia <149967204+wommiez@users.noreply.github.com>
2024-06-18 16:31:14 +0200

    ui

--- SCRIPTS/UI/ui.gd
+++ SCRIPTS/UI/ui.gd
@@ -1,6 +1,14 @@
 extends CanvasLayer
 class_name UI
 
+@onready var main_menu = %MainMenu
+signal start_game()
+
+
+
 #actualizar contadores de llaves 
 #actualizar salud al recibir daño
 #actualizar salud al recoger poción
+
+func _on_main_menu_start_game():
+	start_game.emit()
