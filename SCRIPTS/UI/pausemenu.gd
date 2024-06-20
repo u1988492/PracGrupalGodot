d94dfa97b67a643e55f5495fcdfb593e8c2ee7c1
Claudia <149967204+wommiez@users.noreply.github.com>
2024-06-20 11:38:47 +0200

    ui pausa

--- SCRIPTS/UI/pausemenu.gd
+++ SCRIPTS/UI/pausemenu.gd
@@ -2,12 +2,18 @@ extends Control
 
 @onready var botones = %botones
 
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+@onready var MUSIC_BUS_ID = AudioServer.get_bus_index("MUSIC")
+
 signal start_game()
 
 func _ready():
 	pass
 	
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = !visible
 
 func focus_button():
 	#if botones:
@@ -19,9 +25,7 @@ func focus_button():
 
 
 func _on_play_pressed():
-	pass # Replace with function body.
-
-
+	visible = !visible
 
 func _on_save_pressed():
 	pass # Replace with function body.
@@ -29,4 +33,15 @@ func _on_save_pressed():
 
 
 func _on_exit_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://ESCENAS/UI/MainMenu.tscn")
+
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(MUSIC_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(MUSIC_BUS_ID,  value < .05)
+	
+
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(SFX_BUS_ID,  value < .05)
