c8f33c86e1844e2dbf0426e0453919901e57e6fa
Claudia <149967204+wommiez@users.noreply.github.com>
2024-06-22 10:35:18 +0200

    limpieza

--- ESCENAS/JUEGO/playertext.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-func _on_detective_update_ui():
-	$CurrentHealth.text  = "Current health: " + str(global.playerHealth)
-	$CurrentPos.text = "Current position: " + str(global.playerPositon)
--- SCRIPTS/ITEMS/carta.gd
+++ SCRIPTS/ITEMS/carta.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var carta_numero: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
